100c76892f38913004bc6d652c30fecd7739248d
Nick Martin <nmartin5@buffalo.edu>
2017-02-03 12:37:10 -0500

    Added header files, began file reading

--- evaluate.c
+++ evaluate.c
@@ -1,7 +1,17 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<math.h>
+
+
 int main(int argc, char *argv[]){
-	if (argc != 1){
+	if (argc != 2){
 		std::cout << "error: no input file" << std::endl;
 		return -1;
 	}
-	
+	FILE *fp = fopen(argv[1], "r");
+	int ch = getc(fp);
+	while (ch != EOF) // BEGIN FILE READ
+	{
+		
+	}	
 }
