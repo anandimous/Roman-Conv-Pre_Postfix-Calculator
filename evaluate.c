83faa70c3d692563d621b5f67f0e427a0903871b
anandimous <anandi@buffalo.edu>
2017-02-06 12:21:33 -0500

    Updated eval.c

--- evaluate.c
+++ evaluate.c
@@ -1,17 +1,24 @@
-#include<iostream.h>
 #include<stdio.h>
 #include<math.h>
 
-
 int main(int argc, char *argv[]){
+	//check cmd line args
 	if (argc != 2){
 		std::cout << "error: no input file" << std::endl;
 		return -1;
 	}
+
+	//file validation
 	FILE *fp = fopen(argv[1], "r");
+	if(fp == NULL) {
+		perror("Error opening file: File DNE!");
+		return(-1);
+	}
+
+	//file input stream
 	int ch = getc(fp);
 	while (ch != EOF) // BEGIN FILE READ
 	{
-		
-	}	
+
+	}
 }
