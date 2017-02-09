9046bca54f6f0c77a02cbd6b48961ac3187576ba
Aniruddha Nandi <anandi@buffalo.edu>
2017-02-08 19:26:33 -0500

    Added stack implementation header file

---
+++ cstack.h
@@ -0,0 +1,65 @@
+#include CSTACK_H_
+#define CSTACK_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int node_val;
+  struct node* ptr_next;
+};
+
+/***
+  push()
+  pop()
+  isEmpty()
+  size()
+  */
+
+struct node* head;
+int size_ctr = 0;
+
+void init() {
+  head = NULL;
+}
+
+boolean isEmpty() {
+  if(head == NULL) {
+    return true;
+  }
+  return false;
+}
+
+void push(int inp) {
+    struct node* element = (struct node*) malloc(sizeof(struct node));
+    if(isEmpty() == TRUE){
+      element->node_val = inp;
+      element->ptr_next = NULL;
+      size_ctr = size_ctr + 1;
+    }
+    else {
+      element->node_val = inp;
+      element->ptr_next = head;
+      size_ctr = size_ctr + 1;
+    }
+    head = element;
+}
+
+int pop() {
+  if(isEmpty()) {
+    printf("%sOPERATION ERROR: Nothing to pop; stack is empty!\n", );
+  }
+  else {
+    struct node* rm = head;
+    head = head->next;
+    return rm->node_val;
+    free(rm);
+    size_ctr = size_ctr - 1;
+  }
+}
+
+int size() {
+  return size_ctr;
+}
+
+#endif //cstack.h
