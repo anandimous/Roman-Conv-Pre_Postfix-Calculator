8a3b1fdbbed85c921b42cb5e76423d9405c8e4f5
Nick Martin <nmartin5@buffalo.edu>
2017-02-10 12:47:57 -0500

    Progressed on file reading

--- evaluate.c
+++ evaluate.c
@@ -6,23 +6,94 @@
 
 
 int main(int argc, char *argv[]){
+	int inNameSize;
+	int ch;
+	char* inFile;
+	char* outFile;
+	FILE* fp;
+	char* ret;
+	char* exp = "";
+	const char* matchesNums = " 1234567890";
+	const char* matchesRoman = " IiVvXxLlCcDdMm";
+	const char* matchesSpace = " ";
+	const char* matchesOp = " +-*/()";
+
 	//check cmd line args
-	if (argc != 2){
-		std::cout << "error: no input file" << std::endl;
+	if (argc != 3){
+		std::cout << "error: incorrect number of arguments" << std::endl;
 		return -1;
 	}
 
 	//file validation
-	FILE *fp = fopen(argv[1], "r");
+	
+	// Checks if input file has extension .in
+	ret = strstr(argv[1],".in");
+	if (ret == NULL){
+		inFile = strAppend(argv[1],".in");
+	}
+
+	// Checks if output file has extension .out
+	ret = strstr(argv[2],".out");
+	if (ret == NULL){
+		outFile = strAppend(argv[2],".out");
+	}
+
+	// Open file to input stream fp
+	fp = fopen(inFile, "r");
 	if(fp == NULL) {
 		perror("Error opening file: File DNE!");
-		return(-1);
+		return -1;
 	}
 
-	//file input stream
-	int ch = getc(fp);
+	//file input stream, saves expression as char* to exp
+	ch = getc(fp);
 	while (ch != EOF) // BEGIN FILE READ
 	{
-
+		exp = strAppend(exp,(char*) ch);
+		ch = getc(fp);
 	}
 }
+
+//Concatenates two strings, returns a new string
+char* strAppend(char* str1, char* str2){
+	char * newStr;
+	if ((newStr = malloc(strlen(str1)+strlen(str2)+1)) != NULL){
+		newStr[0] = '\0';
+		strcat(newStr,str1);
+		strcat(newStr,str2);
+		return newStr;
+	}
+	else {
+		perror("malloc failed for strAppend");
+		return "";
+	}
+}
+
+// Returns precedence value of corresponding operator
+int getPrec(char c){
+	if (c == '('){
+		return 3;
+	}
+	else if (c == ')'){
+		return 3;
+	}
+	else if (c == "*"){
+		return 2;
+	}
+	else if (c == "/"){
+		return 2;
+	}
+	else if (c == "+"){
+		return 1;
+	}
+	else if (c == "-"){
+		return 1;
+	}
+	else {
+		//error
+		std::cout << "error: argument is not an operator" << std::endl;
+		return -1;
+	}
+}
+
+
---
+++ evaluate.h
@@ -0,0 +1,7 @@
+#ifndef EVALUATE_H_
+#define EVALUATE_H_
+
+char* strAppend(char* str1, char* str2);
+int getPrec(char c);
+
+#endif // EVALUATE_H_
