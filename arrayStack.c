22e490b3992ab6fed936e5311b9220aa30a5f051
Nick Martin <nmartin5@buffalo.edu>
2017-02-17 20:12:26 -0500

    Stack reformatting

--- arrayStack.c
+++ arrayStack.c
@@ -1,23 +1,24 @@
+//Initialize with pointer to top of stack = 0
 void init(int *top){
   *top = 0;
 }
 
+// Assign the stack[top] -> element, increment top
 void push(char *s, int* top, char element){
   s[(*top)++] = element;
 }
 
+// decrement top pointer, return stack[top]
 char pop(char *s, int *top){
   return s[--(*top)];
 }
 
-int full(int *top, const int size){
-  return *top == size ? 1 : 0;
-}
-
+// return 1(true) if the top pointer is 0
 int empty(int *top){
   return *top == 0 ? 1 : 0;
 }
 
+// return the top element of the stack
 char peek(char*s, int* top){
   return s[*top-1];
 }
--- arrayStack.h
+++ arrayStack.h
@@ -4,7 +4,6 @@
 void init(int *top);
 void push(char *s, int* top, char element);
 char pop(char *s, int *top);
-int full(int *top, const int size);
 int empty(int *top);
 char peek(char*s, int* top);
 
